0f77df7a986bcb4f4f93568a9e82873eccbfc111
Boris Kytsenko <borysigorevich@gmail.com>
2022-07-24 14:04:49 +0300

    abz_july

--- components/Description/Description.tsx
+++ components/Description/Description.tsx
@@ -43,7 +43,8 @@ export const Description = () => {
                     a vast understanding of User design thinking as they'll be building web interfaces with
                     accessibility in
                     mind. They should also be excited to learn, as the world of Front-End Development keeps
-                    evolving.</Typography>
+                    evolving.
+                </Typography>
                 <Button>Sign up</Button>
             </Container>
         </Box>
